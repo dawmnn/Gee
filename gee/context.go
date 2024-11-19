5b1dad4be712cf2f3ee2249da847dbf2f360f0c0
cyh <2263463374@qq.com>
2024-11-19 21:18:12 +0800

    Day 4

--- gee/context.go
+++ gee/context.go
@@ -15,10 +15,15 @@ type Context struct {
 	// request info
 	Path   string
 	Method string
+	Params map[string]string
 	// response info
 	StatusCode int
 }
 
+func (c *Context) Param(key string) string {
+	value, _ := c.Params[key]
+	return value
+}
 func newContext(w http.ResponseWriter, req *http.Request) *Context {
 	return &Context{
 		Writer: w,
