89a9b6236d3d6f793df7c1d8670f954b3deee508
cyh <2263463374@qq.com>
2024-11-25 20:24:14 +0800

    模板

--- gee/gee.go
+++ gee/gee.go
@@ -1,19 +1,22 @@
 package gee
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path"
+	"strings"
 )
 
 // HandlerFunc defines the request handler used by gee
 type HandlerFunc func(*Context)
 
 // Engine implement the interface of ServeHTTP
-type Engine struct {
-	*RouterGroup //继承
-	router       *router
-	groups       []*RouterGroup // store all groups
-}
+//type Engine struct {
+//	*RouterGroup //继承
+//	router       *router
+//	groups       []*RouterGroup // store all groups
+//}
 
 type RouterGroup struct {
 	prefix      string
@@ -76,8 +79,60 @@ func (engine *Engine) POST(pattern string, handler HandlerFunc) {
 func (engine *Engine) Run(addr string) (err error) {
 	return http.ListenAndServe(addr, engine)
 }
-
 func (engine *Engine) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	var middlewares []HandlerFunc
+	for _, group := range engine.groups {
+		if strings.HasPrefix(req.URL.Path, group.prefix) {
+			middlewares = append(middlewares, group.middlewares...)
+		}
+	}
 	c := newContext(w, req)
+	c.handlers = middlewares
+	c.engine = engine
 	engine.router.handle(c)
+}
+func (group *RouterGroup) Use(middlewares ...HandlerFunc) {
+	group.middlewares = append(group.middlewares, middlewares...)
+}
+
+// create static handler
+func (group *RouterGroup) createStaticHandler(relativePath string, fs http.FileSystem) HandlerFunc {
+	absolutePath := path.Join(group.prefix, relativePath)
+	fileServer := http.StripPrefix(absolutePath, http.FileServer(fs)) //访问 /assets/image.png 时，http.StripPrefix 会去掉 /assets，
+	// 然后 http.FileServer 会去 /static/image.png 查找文件并返回
+	return func(c *Context) {
+		file := c.Param("filepath")
+		// Check if file exists and/or if we have permission to access it
+		if _, err := fs.Open(file); err != nil {
+			c.Status(http.StatusNotFound)
+			return
+		}
+
+		fileServer.ServeHTTP(c.Writer, c.Req)
+	}
+}
+
+// serve Static files
+
+func (group *RouterGroup) Static(relativePath string, root string) {
+	handler := group.createStaticHandler(relativePath, http.Dir(root))
+	urlPattern := path.Join(relativePath, "/*filepath")
+	// Register GET handlers
+	group.GET(urlPattern, handler)
+}
+
+type Engine struct {
+	*RouterGroup
+	router        *router
+	groups        []*RouterGroup     // store all groups
+	htmlTemplates *template.Template // for html render
+	funcMap       template.FuncMap   // for html render
+}
+
+func (engine *Engine) SetFuncMap(funcMap template.FuncMap) { //map[string]any
+	engine.funcMap = funcMap
+}
+
+func (engine *Engine) LoadHTMLGlob(pattern string) {
+	engine.htmlTemplates = template.Must(template.New("").Funcs(engine.funcMap).ParseGlob(pattern))
 }
